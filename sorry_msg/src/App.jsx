7ec3de67815c6c81f39309416911883edbb4dc37
Hardik <hardik.pandey.716@gmail.com>
2024-12-13 13:16:50 +0530

    6th commit

--- sorry_msg/src/App.jsx
+++ sorry_msg/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Message from './Components/message'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showSorryMessage, setShowSorryMessage] = useState(true);
 
+  const handleAcknowledge = () => {
+    setShowSorryMessage(false);
+  };
   return (
     <>
-    <p>Hi my name is Hardik</p>
+    <p class="bg-red-600">Hi my name is Hardik</p>
+    <div>
+      {showSorryMessage ? (
+        <SorryMessage
+          message="We're sorry for the inconvenience caused. Please let us know if there's anything we can do to help."
+          onAcknowledge={handleAcknowledge}
+        />
+      ) : (
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-3xl font-semibold">Thanks for understanding!</h1>
+        </div>
+      )}
+    </div>
     </>
   )
 }
