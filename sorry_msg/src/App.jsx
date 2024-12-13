f292b0b2444028f77b5a5d9c7b15c7eea7427880
Hardik <hardik.pandey.716@gmail.com>
2024-12-13 14:10:15 +0530

    last commit

--- sorry_msg/src/App.jsx
+++ sorry_msg/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import Message from './Components/Message';
-import './App.css'
+import BloomingFlower from './Components/BloomingFlower';
+import './App.css';
 
 function App() {
   const [showSorryMessage, setShowSorryMessage] = useState(true);
@@ -8,22 +9,24 @@ function App() {
   const handleAcknowledge = () => {
     setShowSorryMessage(false);
   };
+
   return (
     <>
-    <div>
-      {showSorryMessage ? (
-        <Message
-          message="We're sorry for the inconvenience caused. Please let us know if there's anything we can do to help."
-          onAcknowledge={handleAcknowledge}
-        />
-      ) : (
-        <div className="flex items-center justify-center h-screen">
-          <h1 className="text-3xl font-semibold">Thanks for understanding!</h1>
-        </div>
-      )}
-    </div>
+      <div>
+        {showSorryMessage ? (
+          <Message
+            message="We're sorry for the inconvenience caused. Please let us know if there's anything we can do to help."
+            onAcknowledge={handleAcknowledge}
+          />
+        ) : (
+          <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-semibold mb-4">Thanks for understanding!</h1>
+            <BloomingFlower />
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
